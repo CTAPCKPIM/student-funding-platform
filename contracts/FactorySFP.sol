d05b33a7094ca134f783a8a524316f0557ca990f
Danya <83241890+CTAPCKPIM@users.noreply.github.com>
2025-05-07 18:26:12 +0300

    feat(build): init the foundation of the project (#3)

---
+++ contracts/FactorySFP.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+// import "hardhat/console.sol";
+
+/**
+ * @title Factory of Student Funding Platform
+ * @notice Contract is used to create and manage student funding projects
+ * @author CTAPCKPIM
+ */
+contract FactorySFP {
+    // TODO: init logic of the contract..
+}
