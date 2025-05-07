3b6657eaf45e86d12e113a0c268d7218e2b6ab57
CTAPCKPIM <novickdanya@gmail.com>
2025-05-07 18:22:50 +0300

    feat: build of the project

---
+++ contracts/BeaconSFP.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+// import "hardhat/console.sol";
+
+/**
+ * @title Factory of Student Funding Pool
+ * @notice Contract is used to upgradeable logic for creating student funding pools
+ * @author CTAPCKPIM
+ */
+contract BeaconSFP {
+    // TODO: init logic of the contract..
+}
---
+++ ignition/modules/FactorySFP.ts
@@ -0,0 +1,6 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+// TODO: Add code in future..
