7b0755f70b95a5f4b62f71c4c9ff2e2f90277da2
CTAPCKPIM <novickdanya@gmail.com>
2025-05-10 18:57:31 +0300

    feat(core): add token recovery and refactor validation logic

--- contracts/FactorySFP.sol
+++ contracts/FactorySFP.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import "./BeaconSFP.sol";
-// import "hardhat/console.sol";
+import "./lib/HelperSFP.sol";
 
 /**
  * @title Factory of Student Funding Platform
@@ -13,6 +17,25 @@ import "./BeaconSFP.sol";
  * @author CTAPCKPIM
  */
 contract FactorySFP is Initializable, OwnableUpgradeable {
+    /**
+     * @notice Connect the HelperSFP library to the this contract
+     */
+    using HelperSFP for address;
+    using HelperSFP for uint256;
+    using HelperSFP for string;
+
+    using SafeERC20 for IERC20;
+
+    /**
+     * @notice All errors thrown:
+     * - NotWhitelistedError: Thrown when a user is not whitelisted
+     * - FunctionCallError: Thrown when a function is called with an invalid function signature
+     * - AmountExceedsError: Thrown when the amount exceeds the balance of the address
+     */
+    error NotWhitelistedError();
+    error FunctionCallError();
+    error AmountExceedsError();
+
     /**
      * @notice All variables:
      * { totalProjects } - The total number of projects created
@@ -52,6 +75,7 @@ contract FactorySFP is Initializable, OwnableUpgradeable {
      */
     event BeaconAddressUpdated(address indexed _addressOld, address indexed _addressNew);
     event WhitelistStatusUpdated(address indexed _address, bool _status);
+    event StuckTokensWithdrawn(address indexed _token, uint256 _amount);
     event ProjectCreated(
         uint256 _amount,
         string _name,
@@ -62,34 +86,10 @@ contract FactorySFP is Initializable, OwnableUpgradeable {
     );
 
     /**
-     * @notice Check for zero address
-     */
-    modifier notZeroAddress(address _address) {
-        require(_address != address(0), "Zero address not allowed");
-        _;
-    }
-
-    /**
-     * @notice Check for zero amount
-     */
-    modifier notZeroAmount(uint256 _amount) {
-        require(_amount > 0, "Zero amount not allowed");
-        _;
-    }
-
-    /**
-     * @notice Ckeck for zero string
-     */
-    modifier notZeroString(string memory _string) {
-        require(bytes(_string).length > 0, "Zero string not allowed");
-        _;
-    }
-
-    /**
      * @notice Check if the address is whitelisted
      */
     modifier onlyWhitelisted() {
-        require(whitelist[msg.sender], "Not whitelisted");
+        if (!whitelist[msg.sender]) revert NotWhitelistedError();
         _;
     }
 
@@ -97,7 +97,9 @@ contract FactorySFP is Initializable, OwnableUpgradeable {
      * @notice Initializes the factory
      * @param _beaconAddress The address of the beacon contract
      */
-    function initialize(address _beaconAddress) public initializer notZeroAddress(_beaconAddress) {
+    function initialize(address _beaconAddress) public initializer {
+        _beaconAddress.notZeroAddress();
+
         __Ownable_init(msg.sender);
         beaconAddress = _beaconAddress;
     }
@@ -106,7 +108,9 @@ contract FactorySFP is Initializable, OwnableUpgradeable {
      * @notice Change the beacon address
      * @param _beaconAddress The address of the beacon contract
      */
-    function changeBeaconAddress(address _beaconAddress) public onlyOwner notZeroAddress(_beaconAddress) {
+    function changeBeaconAddress(address _beaconAddress) public onlyOwner {
+        _beaconAddress.notZeroAddress();
+
         emit BeaconAddressUpdated(beaconAddress, _beaconAddress);
         beaconAddress = _beaconAddress;
     }
@@ -116,15 +120,19 @@ contract FactorySFP is Initializable, OwnableUpgradeable {
     * @param _address The address for which to set the status
     * @param _status The status to set (true to add, false to remove)
     */
-    function setWhitelistStatus(address _address, bool _status) public onlyOwner notZeroAddress(_address) {
+    function setWhitelistStatus(address _address, bool _status) public onlyOwner {
+        _address.notZeroAddress();
+        
         whitelist[_address] = _status;
         emit WhitelistStatusUpdated(_address, _status);
     }
 
     /**
-     * TODO: Finish the function after write BeaconSFP contract
-     *
      * @notice Creates a new project using the BeaconProxy pattern
+     * @param _amount The amount of funding pool
+     * @param _name The name of the project
+     * @param _tokenName The name of the token
+     * @param _tokenSymbol The symbol of the token
      * @return projectAddress The address of the newly created project
      */
     function createProject(
@@ -132,13 +140,13 @@ contract FactorySFP is Initializable, OwnableUpgradeable {
         string memory _name,
         string memory _tokenName,
         string memory _tokenSymbol
-    ) public 
-    onlyWhitelisted
-    notZeroAmount(_amount)
-    notZeroString(_name)
-    notZeroString(_tokenName)
-    notZeroString(_tokenSymbol)
-    returns (address) {
+    ) public onlyWhitelisted returns (address) {
+        // Validate the parameters
+        _amount.notZeroAmount();
+        _name.notZeroString();
+        _tokenName.notZeroString();
+        _tokenSymbol.notZeroString();
+
         totalProjects++;
 
         // Create a new BeaconProxy instance using the beacon address
@@ -175,16 +183,34 @@ contract FactorySFP is Initializable, OwnableUpgradeable {
     }
 
     /**
+     * @notice Function to withdraw stuck tokens form the contract
+     * @param _token The address of the token contract
+     * @param _amount The amount of tokens to withdraw
+     */
+    function withdrawStuckTokens(
+        address _token,
+        uint256 _amount
+    ) external onlyOwner {
+        _token.notZeroAddress();
+        _amount.notZeroAmount();
+
+        if (IERC20(_token).balanceOf(address(this)) < _amount) revert AmountExceedsError();
+
+        IERC20(_token).safeTransfer(owner(), _amount);
+        emit StuckTokensWithdrawn(_token, _amount);
+    }
+
+    /**
      * @notice Fallback function to accept native currency
      */
     fallback() external payable {
-        revert("Wrong function called");
+        revert FunctionCallError();
     }
 
     /**
      * @notice Receive function to accept native currency
      */
     receive() external payable {
-        revert("Wrong function called");
+        revert FunctionCallError();
     }
 }
