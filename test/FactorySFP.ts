e57e84d009b980e2d993aebb60ce0c46ece0ca71
CTAPCKPIM <novickdanya@gmail.com>
2025-05-11 01:50:57 +0300

    test(fixture): deploy contracts

--- test/FactorySFP.ts
+++ test/FactorySFP.ts
@@ -1,11 +1,23 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
+import { deployFixture } from "./Fixtures";
 
-describe("...", function () {
-  // TODO: Add test cases for the contract/s..
+describe("FactorySFP", function () {
+    describe("Deployment", function () {
+        it("Should set the right owner", async function () {
+            const { factory, owner } = await loadFixture(deployFixture);
+
+            expect(await factory.owner()).to.equal(owner.address);
+        });
+
+        it("Should set the right beacon address", async function () {
+            const { factory, beaconAddress } = await loadFixture(deployFixture);
+
+            expect(await factory.beaconAddress()).to.equal(beaconAddress);
+        });
+    });
+
+    // TODO: Add tests for the functions in FactorySFP
 });
