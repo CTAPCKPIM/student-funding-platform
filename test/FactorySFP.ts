ff8c43ff6811f10b67fc594bcb36d27bd2009f6f
Danya <83241890+CTAPCKPIM@users.noreply.github.com>
2025-05-12 13:04:11 +0300

    test(factory): full coverage (#24)

--- test/FactorySFP.ts
+++ test/FactorySFP.ts
@@ -2,7 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
-import { deployFixture } from "./Fixtures";
+import { deployFixture, settingsFixture, ZERO_ADDRESS, AMOUNT } from "./Fixtures";
 
 describe("FactorySFP", function () {
     describe("Deployment", function () {
@@ -17,7 +17,360 @@ describe("FactorySFP", function () {
 
             expect(await factory.beaconAddress()).to.equal(beaconAddress);
         });
+
+        it("Should revert if re-initialization is attempted", async function () {
+            const { factory, beaconAddress} = await loadFixture(deployFixture);
+
+            await expect(
+                factory.initialize(beaconAddress)
+            ).to.be.revertedWithCustomError(
+                factory,
+                "InvalidInitialization"
+            );
+        });
     });
 
-    // TODO: Add tests for the functions in FactorySFP
+    // Tests for the changeBeaconAddress();
+    describe("changeBeaconAddress()", function () {
+        it("Only the owner can change beacon address", async function () {
+            const { factory, addr1 } = await loadFixture(deployFixture);
+
+            await expect(
+                factory.connect(addr1).changeBeaconAddress(addr1.address)
+            ).to.be.revertedWithCustomError(
+                factory,
+                "OwnableUnauthorizedAccount"
+            );
+        });
+
+        it("Should revert if the address is zero", async function () {
+            const { factory } = await loadFixture(deployFixture);
+
+            await expect(factory.changeBeaconAddress(
+                ZERO_ADDRESS
+            )).to.be.revertedWithCustomError(
+                factory,
+                "ZeroAddressError"
+            );
+        });
+
+        it("Should change the beacon address", async function () {
+            const { factory, owner, addr1 } = await loadFixture(deployFixture);
+
+            await factory.connect(owner).changeBeaconAddress(addr1.address);
+            expect(await factory.beaconAddress()).to.equal(addr1.address);
+        });
+
+        it("Should emit BeaconAddressChanged event", async function () {
+            const { factory, addr1, beaconAddress } = await loadFixture(deployFixture);
+
+            await expect(factory.changeBeaconAddress(addr1.address))
+                .to.emit(factory, "BeaconAddressChanged")
+                .withArgs(beaconAddress, addr1.address);
+        });
+    });
+
+    // Tests for the setWhitelistStatus(); 
+    describe("setWhitelistStatus()", function () {
+        it("Only the owner can set whitelist status", async function () {
+            const { factory, addr1 } = await loadFixture(deployFixture);
+
+            await expect(
+                factory.connect(addr1).setWhitelistStatus(addr1.address, true)
+            ).to.be.revertedWithCustomError(
+                factory,
+                "OwnableUnauthorizedAccount"
+            );
+        });
+
+        it("Should revert if the address is zero", async function () {
+            const { factory } = await loadFixture(deployFixture);
+
+            await expect(factory.setWhitelistStatus(ZERO_ADDRESS, true)).to.be.revertedWithCustomError(
+                factory,
+                "ZeroAddressError"
+            );
+        });
+
+        it("Should set the whitelist status", async function () {
+            const { factory, addr1 } = await loadFixture(deployFixture);
+
+            await factory.setWhitelistStatus(addr1.address, true);
+            expect(await factory.whitelist(addr1.address)).to.equal(true);
+        });
+
+        it("Should update the whitelist status", async function () {
+            const { factory, addr1 } = await loadFixture(deployFixture);
+
+            await factory.setWhitelistStatus(addr1.address, true);
+            expect(await factory.whitelist(addr1.address)).to.equal(true);
+
+            await factory.setWhitelistStatus(addr1.address, false);
+            expect(await factory.whitelist(addr1.address)).to.equal(false);
+        });
+
+        it("Should emit WhitelistStatusUpdated event", async function () {
+            const { factory, addr1 } = await loadFixture(deployFixture);
+
+            await expect(factory.setWhitelistStatus(addr1.address, true))
+                .to.emit(factory, "WhitelistStatusUpdated")
+                .withArgs(addr1.address, true);
+        });
+    });
+
+    // Tests for the createProject();
+    describe("createProject()", function () {
+        it("Only whitelisted accounts can create projects", async function () {
+            const { factory, addr1 } = await loadFixture(deployFixture);
+
+            // Attempt to create a project without being whitelisted
+            await expect(
+                factory.connect(addr1).createProject(
+                    1000,           // 1000 tokens amount
+                    "Test Project", // Project name
+                    "TT",           // Project tokens symbol
+                    "Test Token"    // Project token name
+                )
+            ).to.be.revertedWithCustomError(
+                factory,
+                "NotWhitelistedError"
+            );
+        });
+
+        it("Should revert if the amount is zero", async function () {
+            const { factory, owner } = await loadFixture(deployFixture);
+
+            // Whitelist the owner
+            await factory.setWhitelistStatus(owner.address, true);
+
+            // Attempt to create a project with a zero amount
+            await expect(
+                factory.connect(owner).createProject(
+                    0,              // 0 tokens amount
+                    "Test Project", // Project name
+                    "TT",           // Project tokens symbol
+                    "Test Token"    // Project token name
+                )
+            ).to.be.revertedWithCustomError(
+                factory,
+                "ZeroAmountError"
+            );
+        });
+
+        it("Should revert if the name is empty", async function () {
+            const { factory, owner } = await loadFixture(deployFixture);
+
+            // Whitelist the owner
+            await factory.setWhitelistStatus(owner.address, true);
+
+            // Attempt to create a project with an empty name
+            await expect(
+                factory.connect(owner).createProject(
+                    1000,           // 1000 tokens amount
+                    "",             // Project name
+                    "TT",           // Project tokens symbol
+                    "Test Token"    // Project token name
+                )
+            ).to.be.revertedWithCustomError(
+                factory,
+                "EmptyStringError"
+            );
+        });
+
+        it("Should revert if the symbol is empty", async function () {
+            const { factory, owner } = await loadFixture(deployFixture);
+
+            // Whitelist the owner
+            await factory.setWhitelistStatus(owner.address, true);
+
+            // Attempt to create a project with an empty symbol
+            await expect(
+                factory.connect(owner).createProject(
+                    1000,           // 1000 tokens amount
+                    "Test Project", // Project name
+                    "",             // Project tokens symbol
+                    "Test Token"    // Project token name
+                )
+            ).to.be.revertedWithCustomError(
+                factory,
+                "EmptyStringError"
+            );
+        });
+
+        it("Should revert if the token name is empty", async function () {
+            const { factory, owner } = await loadFixture(deployFixture);
+
+            // Whitelist the owner
+            await factory.setWhitelistStatus(owner.address, true);
+
+            // Attempt to create a project with a name that is too long
+            await expect(
+                factory.connect(owner).createProject(
+                    1000,           // 1000 tokens amount
+                    "Test Project", // Project name
+                    "TT",           // Project tokens symbol
+                    ""              // Project token name
+                )
+            ).to.be.revertedWithCustomError(
+                factory,
+                "EmptyStringError"
+            );
+        });
+
+        it("Should create a project", async function () {
+            const { factory, owner } = await loadFixture(deployFixture);
+
+            // Whitelist the owner
+            await factory.setWhitelistStatus(owner.address, true);
+
+            // Create a project
+            await factory.connect(owner).createProject(
+                1000,           // 1000 tokens amount
+                "Test Project", // Project name
+                "TT",           // Project tokens symbol
+                "Test Token"    // Project token name
+            );
+
+            const projectAddress = await factory.projects(owner.address, 0);
+            expect(projectAddress).to.not.eq(ZERO_ADDRESS);
+        });
+
+        it("Should emit ProjectCreated event", async function () {
+            const { factory, owner } = await loadFixture(deployFixture);
+
+            // Whitelist the owner
+            await factory.setWhitelistStatus(owner.address, true);
+
+            // Create a project
+            await expect(
+                factory.connect(owner).createProject(
+                    1000,           // 1000 tokens amount
+                    "Test Project", // Project name
+                    "TT",           // Project tokens symbol
+                    "Test Token"    // Project token name
+                )
+            ).to.emit(factory, "ProjectCreated")
+                .withArgs(
+                    1000,           // Amount of tokens
+                    "Test Project", // Project name
+                    "TT",           // Project token symbol
+                    "Test Token",   // Project token name
+                    owner.address,  // Owner address
+                    anyValue
+                );
+        });
+    });
+
+    // Tests for the withdrawStuckTokens();
+    describe("withdrawStuckTokens()", function () {
+        it("Only the owner can withdraw stuck tokens", async function () {
+            const { factory, addr1 } = await loadFixture(settingsFixture);
+
+            // Attempt to withdraw stuck tokens without being the owner
+            await expect(
+                factory.connect(addr1).withdrawStuckTokens(addr1.address, AMOUNT)
+            ).to.be.revertedWithCustomError(
+                factory,
+                "OwnableUnauthorizedAccount"
+            );
+        });
+
+        it("Should revert if the address is zero", async function () {
+            const { factory } = await loadFixture(settingsFixture);
+
+            // Attempt to withdraw stuck tokens with a zero address
+            await expect(
+                factory.withdrawStuckTokens(ZERO_ADDRESS, AMOUNT)
+            ).to.be.revertedWithCustomError(
+                factory,
+                "ZeroAddressError"
+            );
+        });
+
+        it("Should revert if the amount is zero", async function () {
+            const { factory, tokenAddress } = await loadFixture(settingsFixture);
+
+            // Attempt to withdraw stuck tokens with a zero amount
+            await expect(
+                factory.withdrawStuckTokens(tokenAddress, 0)
+            ).to.be.revertedWithCustomError(
+                factory,
+                "ZeroAmountError"
+            );
+        });
+
+        it("Should revert if the amount is greater than the balance", async function () {
+            const { factory, tokenAddress } = await loadFixture(settingsFixture);
+
+            // Attempt to withdraw stuck tokens with an amount greater than the balance
+            await expect(
+                factory.withdrawStuckTokens(tokenAddress, AMOUNT * 2n)
+            ).to.be.revertedWithCustomError(
+                factory,
+                "AmountExceedsError"
+            );
+        });
+
+        it("Should withdraw stuck tokens", async function () {
+            const { factory, token, factoryAddress, tokenAddress } = await loadFixture(settingsFixture);
+
+            // Withdraw the stuck tokens
+            await factory.withdrawStuckTokens(tokenAddress, AMOUNT);
+
+            // Check the balance of the factory
+            const balance = await token.balanceOf(factoryAddress);
+            const ownerBalance = await token.balanceOf(factory.owner());
+
+            expect(balance).to.equal(0);
+            expect(ownerBalance).to.equal(AMOUNT);
+        });
+
+        it("Should emit StuckTokensWithdrawn event", async function () {
+            const { factory, tokenAddress } = await loadFixture(settingsFixture);
+
+            // Withdraw the stuck tokens
+            await expect(
+                factory.withdrawStuckTokens(tokenAddress, AMOUNT)
+            ).to.emit(factory, "StuckTokensWithdrawn")
+                .withArgs(tokenAddress, AMOUNT);
+        });
+    });
+
+    // Test for the fallback();
+    describe("fallback()", function () {
+        it("Should revert when called with Ether and arbitrary data", async function () {
+            const { factory, owner } = await loadFixture(deployFixture);
+
+            await expect(
+                owner.sendTransaction({
+                    to: await factory.getAddress(),
+                    value: AMOUNT / 10n,
+                    data: "0x1234567890"
+                })
+            ).to.be.revertedWithCustomError(
+                factory,
+                "FunctionCallError"
+            );
+        });
+    });
+
+    // Test for the receive();
+    describe("receive()", function () {
+        it("Should revert when receiving native currency without data", async function () {
+             const { factory, owner } = await loadFixture(deployFixture);
+
+            await expect(
+                owner.sendTransaction({
+                    to: await factory.getAddress(),
+                    value: AMOUNT / 10n,
+                    data: "0x"
+                })
+            ).to.be.revertedWithCustomError(
+                factory,
+                "FunctionCallError"
+            );
+        });
+    });
 });
+
+
--- test/Fixtures.ts
+++ test/Fixtures.ts
@@ -5,6 +5,12 @@
 import { ethers, upgrades } from "hardhat";
 
 /**
+ * @notice All variables used in the tests
+ */
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+export const AMOUNT = ethers.parseEther("1000");
+
+/**
  * @notice Get addresses of the signers
  */
 export const getAddress = async () => {
@@ -22,6 +28,7 @@ export const deployFixture = async () => {
     // Get contracts factories
     const FactorySFP = await ethers.getContractFactory("FactorySFP");
     const BeaconSFP = await ethers.getContractFactory("BeaconSFP");
+    const Token = await ethers.getContractFactory("Token");
 
     // Deploy the BeaconSFP contract
     const beacon = await upgrades.deployBeacon(BeaconSFP);
@@ -33,12 +40,44 @@ export const deployFixture = async () => {
     await factory.waitForDeployment();
     const factoryAddress = await factory.getAddress();
 
+    // Deploy the Token contract
+    const token = await upgrades.deployProxy(Token, [ "Ukraine University", "UU" ]);
+    await token.waitForDeployment();
+    const tokenAddress = await token.getAddress();
+
     return {
         owner,
         factory,
         beacon,
+        token,
         factoryAddress,
         beaconAddress,
+        tokenAddress,
         ...rest
     };
+
 }
+
+/**
+ * @notice Set and get settings contracts
+ */
+export const settingsFixture = async () => {
+    const { owner, addr1, factory, factoryAddress, token, tokenAddress, ...rest } = await deployFixture();
+
+    // Whitelist the adddress
+    await factory.setWhitelistStatus(owner.address, true);
+    await factory.setWhitelistStatus(addr1.address, true);
+
+    // Mint the token to FactorySFP
+    await token.connect(owner).mint(factoryAddress, AMOUNT);
+
+    return {
+        owner,
+        addr1,
+        factory,
+        factoryAddress,
+        token,
+        tokenAddress,
+        ...rest
+    };
+};
