36179e257fdf0165a71bc903776c3d57d67c448b
Danya <83241890+CTAPCKPIM@users.noreply.github.com>
2025-05-14 11:50:55 +0300

    feat(config): set for deploy (#35)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import 'solidity-coverage';
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -12,6 +13,19 @@ const config: HardhatUserConfig = {
         runs: 200
       }
     }
+  },
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+    },
   },
 };
 
